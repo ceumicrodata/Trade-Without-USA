bde89a42bfef5c65f46b86f05c72b1beacc6a21f
Miklós Koren <miklos.koren@gmail.com>
2024-09-20 21:50:29 +0200

    Compute standard errors across 10 runs

---
+++ sterr.jl
@@ -0,0 +1,48 @@
+using CSV
+using DataFrames
+using Statistics
+
+seeds = [
+    7018
+    1201
+    6503
+    5695
+    1499
+    5199
+    6177
+    5469
+    6494
+    2432    
+]
+
+function readfile(seed)
+    CSV.read("output/$seed.csv", DataFrame)
+end
+
+function extract(dfs, column)
+    K = length(dfs)
+    N = nrow(dfs[1])
+    out = zeros(N, K)
+    for k = 1:K
+        out[:, k] = dfs[k][:, column]
+    end
+    out
+end
+
+stats(A) = [mean(A, dims=2) std(A, dims=2)]
+
+function main()
+    dfs = readfile.(seeds)
+    trade_barriers = extract(dfs, :trade_barriers) |> stats
+    diversification = extract(dfs, :diversification) |> stats
+
+    df = DataFrame(country=dfs[1].country_names, 
+        trade_barriers_mean=trade_barriers[:, 1], 
+        trade_barriers_se=trade_barriers[:, 2],
+        diversification_mean=diversification[:, 1], 
+        diversification_se=diversification[:, 2],
+    )
+    CSV.write("output/sterr.csv", df)
+end
+
+main()
